fe2f7852c4ce7bac5152c069489c3f01086c0304
Christopher <christo.navas@hotmail.fr>
2024-12-22 21:58:01 +0100

    init project with uploadedImage

--- app/page.js
+++ app/page.js
@@ -1,101 +1,124 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import cn from "classnames";
+
+const Settings = ({ setImage, setSettings, className, settings }) => {
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImage({
+        src: reader.result, // Ici, nous stockons l'image en base64
+      });
+    };
+
+    if (file) {
+      reader.readAsDataURL(file); // Convertit le fichier en base64
+    }
+  };
+
+  return (
+    <div className={cn("card bg-neutral", className)}>
+      <div className="card-body">
+        <h1 className="text-2xl pb-2">Settings</h1>
+        <form className="flex flex-col gap-5">
+          <div className="flex gap-2 flex-col">
+            <p className="w-full">File</p>
+            <input
+              type="file"
+              onChange={handleFileChange}
+              className="bg-primary file-input w-full max-w-xs"
+            />
+          </div>
+          <div className="flex gap-2 flex-col">
+            <p className="w-full">Padding</p>
+            <input
+              type="range"
+              min={0}
+              max="99"
+              value={10}
+              className="range range-primary"
+            />
+          </div>
+          <div className="flex gap-2 flex-col">
+            <p className="w-full">Shadow</p>
+            <input
+              type="range"
+              min={0}
+              max="99"
+              value="40"
+              className="range range-primary"
+            />
+          </div>
+          <div className="flex gap-2 flex-col">
+            <p className="w-full">Radius</p>
+            <input
+              type="range"
+              min={0}
+              max="99"
+              value="40"
+              className="range range-primary"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+/* eslint-disable @next/next/no-img-element */
+const ImageGenerator = (props) => {
+  console.log(props.image);
+  if (!props.image) {
+    alert("Pas d'image");
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        // Ajoute le padding
+      }}
+    >
+      <img
+        src={props.image.src}
+        alt="Image"
+        style={{
+          // Ajoute le border-radius et le boxShadow
+          boxShadow: `0 0 ${props.settings.shadow}px rgba(0,0,0,.${props.settings.shadow})`,
+          borderRadius: `${props.settings.radius}px`,
+        }}
+      />
+    </div>
+  );
+};
 
 export default function Home() {
+  const [image, setImage] = useState();
+  const [settings, setSettings] = useState({
+    padding: 0,
+    borderRadius: 0,
+    shadow: 0,
+  });
+  console.log(image);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="max-w-4xl m-auto  ">
+      <div className="flex items-center gap-x-20 justify-center min-h-screen w-full">
+        <Settings
+          setSettings={setSettings}
+          setImage={setImage}
+          className="flex-1 w-1/2"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex-1 text-center w-1/2 h-fit border p-5 rounded-md">
+          {image ? (
+            <ImageGenerator settings={settings} image={image} />
+          ) : (
+            <p>Upload an image first.</p>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
